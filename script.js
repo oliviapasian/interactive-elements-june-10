cb44f9efdbda3a1d36884c2c194cc8f90d82084f
oliviapasian <99832146+oliviapasian@users.noreply.github.com>
2022-06-10 20:18:25 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Clock
+var dt = new Date();
+document.getElementById("datetime").innerHTML = dt.toLocaleTimeString();
+
+//Welcome Alert
+alert("Welcome! Relax and stay a while! Check back soon to see a new interaction...");
